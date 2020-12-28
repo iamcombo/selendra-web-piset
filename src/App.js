6152d4a40e396c19f2e2e762dedbae24e8a922b4
COMBO <piset2cambo@gmail.com>
2020-12-28 08:07:12 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { 
+  Home, 
+  Wallet, 
+  About, 
+  MkPlace, 
+  Privacy, 
+  TermOfUse, 
+  Success 
+} from './pages';
+import { 
+  Header, 
+  Notify, 
+  Footer 
+} from './components';
+import {
+  BrowserRouter as
+  Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/wallet' exact component={Wallet} />
+            <Route path='/about' exact component={About} />
+            <Route path='/mkplace' exact component={MkPlace} />
+            <Route path='/privacy' exact component={Privacy} />
+            <Route path='/termofuse' exact component={TermOfUse} />
+            <Route path='/successfullyverified' exact component={Success} />
+            {/* 
+            <Route path='/failedverified' exact component={Failedverified} /> 
+            */}
+          </Switch>
+        <Notify />
+        <Footer />
+      </Router>
+    </div>
+  );
+}
